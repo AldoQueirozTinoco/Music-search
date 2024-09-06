1a2ef98935407da7e9e910cc91ca6ecf0af7997d
Aldo Queiroz <149337171+AldoQueirozTinoco@users.noreply.github.com>
2024-09-06 10:55:59 -0300

    Add project files

---
+++ app.js
@@ -0,0 +1,97 @@
+
+//console.log(dados)
+
+/*
+
+let resultados = ""
+
+//Para cada result dentro da lista
+for(let dado of dados){
+    resultados += `
+    <div class="item-resultado">
+    
+    <h2><a href="#">${dado.titulo}</a></h2>
+    <p class="descricao-meta">${dado.descricao}
+    </p>
+    <p class="descricao-meta">Album: ${dado.album}</p>
+    <a href="${dado.link}" target="blank">Mais informações</a>
+    </div>
+    `
+    }
+
+    section.innerHTML = resultados;
+
+*/
+
+    function pesquisar() {
+        /**
+         * Função responsável por renderizar os resultados da pesquisa na página.
+         *
+         * Percorre o array `dados` e para cada objeto, cria um elemento HTML
+         * com as informações do item encontrado.
+         * 
+         * Se o valor for nulo ou se o mesmo for composto apenas de espaços a página
+         * retorna "Nada foi encontrado"
+         *
+         * **Parâmetros:**
+         *  - Nenhum
+         *
+         * **Retorno:**
+         *  - Não retorna nenhum valor, mas modifica o DOM inserindo os elementos HTML.
+         */
+      
+        let section = document.getElementById("resultados-pesquisa"); // Obtém a seção onde os resultados serão inseridos
+        let campoPesquisa = document.getElementById("campo-pesquisa").value;
+        
+        let resultados = "";
+        let titulo = "";
+        let descricao = ""; 
+        let album = "";
+
+        if(!campoPesquisa || isOnlySpaces(campoPesquisa)){
+          section.innerHTML = "<p>Nada foi encontrado</p>"
+          return
+        }
+
+      campoPesquisa = campoPesquisa.toLowerCase();
+          dados.forEach(dado => {
+            /**
+             * Para cada objeto `dado` no array `dados`:
+             *  - Cria uma string HTML com as informações do item.
+             *  - Insere o HTML criado no final da seção de resultados.
+             */
+
+            titulo = dado.titulo.toLowerCase();
+            descricao = dado.descricao.toLowerCase();
+            album = dado.album.toLowerCase();
+
+          if(titulo.includes(campoPesquisa) ||
+           descricao.includes(campoPesquisa) ||
+            album.includes(campoPesquisa)){
+            resultados += `
+            <div class="item-resultado">
+            <h2><a href="Albums/Help.jpg" target="blank">${dado.titulo}</a></h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <p class="descricao-meta">Album: ${dado.album}</p>
+            <a href="${dado.link}" target="blank">Mais informações</a>
+            </div>
+            `;
+          }
+       
+          }
+        );
+  
+        if(!resultados){
+          resultados = "<p>Nada foi encontrado. Você não buscou por musicas</p>"
+        }
+
+          section.innerHTML= resultados;
+    }
+    //Função testa se o texto digitado na barra de pesquisa é composto somente de espaços
+    function isOnlySpaces(str) {
+      return /^\s+$/.test(str);
+      // Utilização de expressão regular.
+      //^ e $ como início e fim do string respectivamente
+      // \s representa qualquer caractere de espaço em branco (espao, tabs etc)
+      // O método test() retorna true se a string corresponder a expressão regular
+    }
---
+++ dados.js
@@ -0,0 +1,62 @@
+
+
+let dados = [
+    {
+        titulo: "Help!",
+        descricao: "Help! dos Beatles é uma canção icônica que vai além de ser apenas uma melodia catchy. Lançada em 1965, a música reflete um momento de introspecção de John Lennon, que se sentia sobrecarregado pela fama e as responsabilidades da Beatlemania. ",
+        link: "https://pt.wikipedia.org/wiki/Help!",
+        album: "Help!",
+        ano: 1965,
+    },
+    {
+        titulo: "Yellow Submarine",
+        descricao: "Yellow Submarine é uma canção psicodélica dos Beatles que se tornou um dos maiores sucessos da banda. A música, com sua letra surreal e melodia alegre, foi inspirada em um desenho animado que os Beatles estavam produzindo na época.",
+        link: "https://pt.wikipedia.org/wiki/Yellow_Submarine",
+        album: "Yellow Submarine",
+        ano: 1966,
+    },
+    {
+        titulo: "The Fool on the Hill",
+        descricao: "The Fool on the Hill é uma canção dos Beatles com uma atmosfera mística e introspectiva. A letra, escrita por Paul McCartney, fala sobre um sábio que observa o mundo de um ponto de vista elevado, buscando significado e compreensão.",
+        link: "https://pt.wikipedia.org/wiki/The_Fool_on_the_Hill",
+        album: "Magical Mystery Tour",
+        ano: 1967,
+    },
+        {
+            titulo: "Yesterday",
+            descricao: "Uma das músicas mais conhecidas e gravadas de todos os tempos, 'Yesterday' é uma balada melancólica composta por Paul McCartney. A letra simples e a melodia inesquecível a tornaram um clássico da música popular.",
+            link: "https://pt.wikipedia.org/wiki/Yesterday",
+            album: "Help!",
+            ano: 1965,
+        },
+        {
+            titulo: "Let It Be",
+            descricao: "Lançada em 1970, 'Let It Be' se tornou um hino de paz e esperança. A música reflete um período de turbulência nos Beatles, mas também transmite uma mensagem de positividade e união.",
+            link: "https://pt.wikipedia.org/wiki/Let_It_Be",
+            album: "Let It Be",
+            ano: 1970,
+            
+        },
+        {
+            titulo: "Strawberry Fields Forever",
+            descricao: "Uma das músicas mais experimentais dos Beatles, 'Strawberry Fields Forever' é conhecida por seus efeitos sonoros e sua letra surreal. Foi composta por John Lennon e Paul McCartney.",
+            link: "https://pt.wikipedia.org/wiki/Strawberry_Fields_Forever",
+            album: "Sgt Peppers Lonely Hearts Club Band",
+            ano: 1967,
+            
+        },
+        {
+            titulo: "Lucy in the Sky with Diamonds",
+            descricao: "Outra música psicodélica dos Beatles, 'Lucy in the Sky with Diamonds' é famosa por sua letra imaginativa e seus arranjos orquestrais. A música foi inspirada em um desenho feito pelo filho de John Lennon.",
+            link: "https://pt.wikipedia.org/wiki/Lucy_in_the_Sky_with_Diamonds",
+            album: "Sgt Peppers Lonely Hearts Club Band",
+            ano: 1967,
+        },
+        {
+            titulo: "Flying",
+            descricao: "Uma música instrumental dos Beatles, 'Flying' é uma composição de George Harrison. A música apresenta um ritmo animado e uma guitarra melódica.",
+            link: "https://pt.wikipedia.org/wiki/Flying_(Beatles)",
+            album: "Magical Mystery Tour",
+            ano: 1967,
+        }
+    ];
